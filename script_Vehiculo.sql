49023e6709c63db6b912896ea7c98a8bc6df0b0b
Junior27VQ <favq1999@outlook.com>
2026-05-18 13:53:41 -0500

    new scripts.sql

---
+++ script_Vehiculo.sql
@@ -0,0 +1,9 @@
+create table vehiculo(
+	placa varchar(10) primary key,
+	marca varchar(50) not null,
+	modelo varchar(50) not null,
+	anio int not null,
+	precio decimal not null,
+	color varchar(30),
+	disponible boolean not null
+);
